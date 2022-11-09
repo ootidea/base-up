25135b04a49514a160fb0319415718fd5aa222e4
ootidea <ootidea@gmail.com>
2022-11-09 21:14:39 +0900

    feat: try Vitest instead of Jest for performance

--- src/object/other.test.ts
+++ src/object/other.test.ts
@@ -1,8 +1,8 @@
 import { fromEntries, keys, numberKeys } from './other'
 
 test('keys', () => {
-  expect(keys({ abc: 3, def: true })).toBe(['abc', 'def'])
-  expect(keys({ 0: '', 1: undefined, abc: null })).toBe(['0', '1', 'abc'])
+  expect(keys({ abc: 3, def: true })).toStrictEqual(['abc', 'def'])
+  expect(keys({ 0: '', 1: undefined, abc: null })).toStrictEqual(['0', '1', 'abc'])
 })
 
 test('fromEntries', () => {
@@ -19,5 +19,5 @@ test('fromEntries', () => {
 })
 
 test('numberKeys', () => {
-  expect(numberKeys({ 10: [], 4: 'abc' })).toBe([10, 4])
+  expect(numberKeys({ 10: [], 4: 'abc' })).toStrictEqual([10, 4])
 })
