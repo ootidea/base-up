0977a2226e9367e8f019cbf32e08f460b4d9025d
ootidea <ootidea@gmail.com>
2022-11-09 21:52:39 +0900

    feat: define isJust function

--- src/other.ts
+++ src/other.ts
@@ -1,4 +1,3 @@
-
 export function assert<T, U extends T>(value: T, predicate: (value: T) => value is U): asserts value is U
 export function assert<T>(value: T, predicate: (value: T) => boolean): void | never
 export function assert<T>(value: T, predicate: (value: T) => boolean): void | never {
@@ -7,3 +6,18 @@ export function assert<T>(value: T, predicate: (value: T) => boolean): void | ne
   }
 }
 
+/**
+ * Generate a function with Type predicate that checks for equality with the given value.
+ * @example As an equivalence function
+ * isJust(123)(456) results false
+ * isJust(null)(null) results true
+ * @example As a type guard function
+ * let n = 123
+ * assert(n, isJust(456)) will throw an exception.
+ * assert(n, isJust(123 as const)) will narrow the type of n to 123.
+ */
+export const isJust =
+  <T, U extends T>(literal: U) =>
+  (value: T): value is U => {
+    return value === literal
+  }
