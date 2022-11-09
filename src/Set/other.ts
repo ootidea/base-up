60c57d6b1434e83be3be25c66b85669997a36e8e
ootidea <ootidea@gmail.com>
2022-11-09 22:40:36 +0900

    feat: improve Set function type

--- src/Set/other.ts
+++ src/Set/other.ts
@@ -5,20 +5,20 @@ export function map<T, U>(set: Set<T>, f: (_: T) => U): Set<U> {
   return new Set(mapIterator(set.values(), f))
 }
 
-export function union<T>(lhs: Set<T>, rhs: Set<T>): Set<T> {
-  const cloned = new Set(lhs)
+export function union<T, U>(lhs: Set<T>, rhs: Set<U>): Set<T | U> {
+  const cloned = new Set<T | U>(lhs)
   for (const value of rhs.values()) {
     cloned.add(value)
   }
   return cloned
 }
 
-export function intersection<T>(lhs: Set<T>, rhs: Set<T>): Set<T> {
-  const result = new Set<T>()
+export function intersection<T, U>(lhs: Set<T>, rhs: Set<U>): Set<T & U> {
+  const result = new Set<T & U>()
   const [small, big] = sortBy([lhs, rhs], (set) => set.size)
   for (const value of small.values()) {
-    if (big.has(value)) {
-      result.add(value)
+    if (big.has(value as any)) {
+      result.add(value as any)
     }
   }
   return result
