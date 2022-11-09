b6821c3502df3ff09fe546ad60098702593ee22d
ootidea <ootidea@gmail.com>
2022-11-09 20:25:44 +0900

    feat: define Set union function

--- src/Set/other.test.ts
+++ src/Set/other.test.ts
@@ -1,5 +1,9 @@
-import { map } from './other'
+import { map, union } from './other'
 
 test('map', () => {
   expect([...map(new Set([2, 1, 3]), (x) => x + 10)]).toStrictEqual([12, 11, 13])
+})
+
+test('union', () => {
+  expect([...union(new Set([1, 2, 3]), new Set([3, 4, 5]))]).toStrictEqual([1, 2, 3, 4, 5])
 })
--- src/Set/other.ts
+++ src/Set/other.ts
@@ -3,3 +3,11 @@ import { map as mapIterator } from '../Iterator/other'
 export function map<T, U>(set: Set<T>, f: (_: T) => U): Set<U> {
   return new Set(mapIterator(set.values(), f))
 }
+
+export function union<T>(lhs: Set<T>, rhs: Set<T>): Set<T> {
+  const cloned = new Set(lhs)
+  for (const value of rhs.values()) {
+    cloned.add(value)
+  }
+  return cloned
+}
