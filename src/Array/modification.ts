bedf6f9a89c5f366522e217f2587dccf7aee72f8
ootidea <ootidea@gmail.com>
2022-11-10 21:54:53 +0900

    feat: define tail function

--- src/Array/modification.ts
+++ src/Array/modification.ts
@@ -1,3 +1,15 @@
+import { ReadonlyNonEmptyArray } from './type'
+
+export function tail<T>(self: ReadonlyNonEmptyArray<T>): readonly T[]
+export function tail<T>(self: readonly T[]): readonly T[] | undefined
+export function tail<T>(self: readonly T[]): readonly T[] | undefined {
+  if (self.length === 0) return undefined
+
+  const cloned = self.slice()
+  cloned.shift()
+  return cloned
+}
+
 export function removeAt<T>(array: readonly T[], i: number): readonly T[] {
   const cloned = array.slice()
   cloned.splice(i, 1)
