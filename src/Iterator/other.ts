0197dd17986697828833f4b07f06636c02d80ecc
ootidea <ootidea@gmail.com>
2022-11-10 18:37:14 +0900

    refactor: rename to self

--- src/Iterator/other.ts
+++ src/Iterator/other.ts
@@ -1,15 +1,15 @@
-export function* map<T, U>(iterable: Iterable<T>, f: (_: T) => U): Generator<U> {
-  const iterator = iterable[Symbol.iterator]()
+export function* map<T, U>(self: Iterable<T>, f: (_: T) => U): Generator<U> {
+  const iterator = self[Symbol.iterator]()
   for (let value = iterator.next(); !value.done; value = iterator.next()) {
     yield f(value.value)
   }
   iterator.return?.()
 }
 
-export function filter<T, U extends T>(iterable: Iterable<T>, f: (_: T) => _ is U): Generator<U>
-export function filter<T>(iterable: Iterable<T>, f: (_: T) => boolean): Generator<T>
-export function* filter<T>(iterable: Iterable<T>, f: (_: T) => boolean): Generator<T> {
-  const iterator = iterable[Symbol.iterator]()
+export function filter<T, U extends T>(self: Iterable<T>, f: (_: T) => _ is U): Generator<U>
+export function filter<T>(self: Iterable<T>, f: (_: T) => boolean): Generator<T>
+export function* filter<T>(self: Iterable<T>, f: (_: T) => boolean): Generator<T> {
+  const iterator = self[Symbol.iterator]()
   for (let value = iterator.next(); !value.done; value = iterator.next()) {
     if (f(value.value)) {
       yield value.value
