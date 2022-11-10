a8d3b3c313fe40c2cb0242088ff5fea3809daa9e
ootidea <ootidea@gmail.com>
2022-11-10 11:46:24 +0900

    feat: improve Set function type

--- src/Set/other.ts
+++ src/Set/other.ts
@@ -1,11 +1,11 @@
 import { sortBy } from '../Array/order'
 import { map as mapIterator } from '../Iterator/other'
 
-export function map<T, U>(set: Set<T>, f: (_: T) => U): Set<U> {
+export function map<T, U>(set: ReadonlySet<T>, f: (_: T) => U): ReadonlySet<U> {
   return new Set(mapIterator(set.values(), f))
 }
 
-export function union<T, U>(lhs: Set<T>, rhs: Set<U>): Set<T | U> {
+export function union<T, U>(lhs: ReadonlySet<T>, rhs: ReadonlySet<U>): ReadonlySet<T | U> {
   const cloned = new Set<T | U>(lhs)
   for (const value of rhs.values()) {
     cloned.add(value)
@@ -13,7 +13,7 @@ export function union<T, U>(lhs: Set<T>, rhs: Set<U>): Set<T | U> {
   return cloned
 }
 
-export function intersection<T, U>(lhs: Set<T>, rhs: Set<U>): Set<T & U> {
+export function intersection<T, U>(lhs: ReadonlySet<T>, rhs: ReadonlySet<U>): ReadonlySet<T & U> {
   const result = new Set<T & U>()
   const [small, big] = sortBy([lhs, rhs], (set) => set.size)
   for (const value of small.values()) {
