1b3e8a992949040998170f66e3f6130a708f8acf
ootidea <ootidea@gmail.com>
2022-11-11 14:09:27 +0900

    feat: define Array map

--- src/Array/basic.ts
+++ src/Array/basic.ts
@@ -12,6 +12,12 @@ export function last<T>(array: readonly T[]): T | undefined {
   return array[array.length - 1]
 }
 
+export function map<T, U>(self: ReadonlyNonEmptyArray<T>, f: (_: T) => U): ReadonlyNonEmptyArray<U>
+export function map<T, U>(self: readonly T[], f: (_: T) => U): readonly U[]
+export function map<T, U>(self: readonly T[], f: (_: T) => U): readonly U[] {
+  return self.map(f)
+}
+
 export function filter<T, U extends T>(array: readonly T[], f: (_: T) => _ is U): readonly U[]
 export function filter<T>(array: readonly T[], f: (_: T) => boolean): readonly T[]
 export function filter<T>(array: readonly T[], f: (_: T) => boolean): readonly T[] {
