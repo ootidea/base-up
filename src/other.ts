8653ed28577ff03769fc8721fe2b209e6821bb06
ootidea <ootidea@gmail.com>
2022-11-12 16:39:55 +0900

    feat: define assertEquals

--- src/other.ts
+++ src/other.ts
@@ -14,27 +14,20 @@ export type PseudoAny = null | undefined | void | boolean | number | bigint | st
 export type Tuple = readonly PseudoAny[]
 
 export function assert<T, U extends T>(value: T, predicate: (value: T) => value is U): asserts value is U
-export function assert<T>(value: T, predicate: (value: T) => boolean): void | never
-export function assert<T>(value: T, predicate: (value: T) => boolean): void | never {
+export function assert<T>(value: T, predicate: (value: T) => boolean): void
+export function assert<T>(value: T, predicate: (value: T) => boolean) {
   if (predicate(value)) {
     throw new Error()
   }
 }
 
-/**
- * Generate a function with Type predicate that checks for equality with the given value.
- * @example As an equivalence function
- * isJust(123)(456) results false
- * isJust(null)(null) results true
- * @example As a type guard function
- * let n = 123
- * assert(n, isJust(456)) will throw an exception.
- * assert(n, isJust(123 as const)) will narrow the type of n to 123.
- */
-export const isJust =
-  <T extends PseudoAny, U extends T>(literal: U) =>
-  (value: T): value is U =>
-    value === literal
+export function assertEquals<T extends PseudoAny, U extends T>(lhs: T, rhs: U): asserts lhs is U
+export function assertEquals<T extends U, U extends PseudoAny>(lhs: T, rhs: U): asserts rhs is T
+export function assertEquals<T, U>(lhs: T, rhs: U) {
+  if (lhs !== (rhs as any)) {
+    throw new Error()
+  }
+}
 
 export type nullish = null | undefined
 
