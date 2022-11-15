5510d59622e74cea35279e813186b2a5a4b12e02
ootidea <ootidea@gmail.com>
2022-11-15 21:36:18 +0900

    feat: define Abs, Neg, Min, Max, RangeTo types

--- src/number.ts
+++ src/number.ts
@@ -1,4 +1,4 @@
-import { AccurateTuple, ReadonlyNonEmptyArray } from './Array'
+import { AccurateTuple, FixedSizeArray, OrMoreSizeArray, ReadonlyNonEmptyArray, Tuple } from './Array'
 
 /**
  * @example
@@ -12,6 +12,65 @@ export type Until<N extends number> = number extends N ? number : N extends N ? 
 type _Until<N extends number, Result extends AccurateTuple = []> = Result['length'] extends N
   ? never
   : Result['length'] | _Until<N, [...Result, any]>
+
+/**
+ * @example
+ * Abs<-3> is equivalent to 3
+ * Abs<0.12> is equivalent to 0.12
+ * Abs<-0> is equivalent to 0
+ */
+export type Abs<N extends number> = `${N}` extends `-${infer P extends number}` ? P : N
+/**
+ * @example
+ * Neg<1> is equivalent to -1
+ * Neg<-0.5> is equivalent to 0.5
+ */
+export type Neg<N extends number> = `${N}` extends `-${infer P extends number}`
+  ? P
+  : `-${N}` extends `${infer M extends number}`
+  ? M
+  : never
+
+/**
+ * @example
+ * Min<0, 3> is equivalent to 0
+ * Min<2, 2> is equivalent to 2
+ */
+export type Min<N extends number, M extends number> = OrMoreSizeArray<N> extends OrMoreSizeArray<M> ? M : N
+/**
+ * @example
+ * Max<0, 3> is equivalent to 3
+ * Max<2, 2> is equivalent to 2
+ */
+export type Max<N extends number, M extends number> = OrMoreSizeArray<N> extends OrMoreSizeArray<M> ? N : M
+
+/**
+ * @example
+ * RangeTo<3> is equivalent to 0 | 1 | 2
+ * RangeTo<4, 8> is equivalent to 4 | 5 | 6 | 7
+ * RangeTo<5, 3> is equivalent to 5 | 4
+ * RangeTo<1, 2> is equivalent to 1
+ * RangeTo<1, 1> is equivalent to never
+ * RangeTo<0> is equivalent to never
+ * RangeTo<2 | 4> is equivalent to 0 | 1 | 2 | 3
+ * RangeTo<number, 9> is equivalent to number
+ * RangeTo<9, number> is equivalent to number
+ */
+export type RangeTo<N extends number, M extends number = 0> = PositiveRangeTo<N, M>
+type PositiveRangeTo<N extends number, M extends number = 0> = number extends N
+  ? number
+  : number extends M
+  ? number
+  : N extends N
+  ? M extends M
+    ? _PositiveRangeTo<Min<N, M>, Max<N, M>>
+    : never
+  : never
+type _PositiveRangeTo<
+  Min extends number,
+  Max extends number,
+  Result extends Tuple = FixedSizeArray<Min>
+> = Result['length'] extends Max ? never : Result['length'] | _PositiveRangeTo<Min, Max, [...Result, any]>
 
 /**
  * @example
