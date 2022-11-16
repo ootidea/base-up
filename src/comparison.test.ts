410a857e75c9b14a7b980d943d00d07db09e8f98
ootidea <ootidea@gmail.com>
2022-11-16 16:03:20 +0900

    feat: define lexicographicLte function

--- src/comparison.test.ts
+++ src/comparison.test.ts
@@ -1,9 +1,20 @@
-import { lexicographicLt } from './comparison'
+import { lexicographicLt, lexicographicLte } from './comparison'
 
 test('lexicographicLt', () => {
   expect(lexicographicLt([1, 2, 3], [1, 8, 0])).toStrictEqual(true)
   expect(lexicographicLt([1, 2, 3], [1, 2, 3])).toStrictEqual(false)
   expect(lexicographicLt([1, 2, 3], [1, 2, 3, 4])).toStrictEqual(true)
+  expect(lexicographicLt([1, 2, 3], [1, 2])).toStrictEqual(false)
   expect(lexicographicLt([1, 2, 3], [5, 7])).toStrictEqual(true)
+  expect(lexicographicLt([1, 2, 3], [0, 7])).toStrictEqual(false)
   expect(lexicographicLt([], [])).toStrictEqual(false)
+})
+
+test('lexicographicLte', () => {
+  expect(lexicographicLte([1, 2, 3], [1, 8, 0])).toStrictEqual(true)
+  expect(lexicographicLte([1, 2, 3], [1, 2, 3])).toStrictEqual(true)
+  expect(lexicographicLte([1, 2, 3], [1, 2, 3, 4])).toStrictEqual(true)
+  expect(lexicographicLte([1, 2, 3], [5, 7])).toStrictEqual(true)
+  expect(lexicographicLte([1, 2, 3], [0, 7])).toStrictEqual(false)
+  expect(lexicographicLte([], [])).toStrictEqual(true)
 })
--- src/comparison.ts
+++ src/comparison.ts
@@ -37,3 +37,18 @@ export function lexicographicLt<T>(lhs: Iterable<T>, rhs: Iterable<T>): boolean 
   }
   return false
 }
+
+export function lexicographicLte<T>(lhs: Iterable<T>, rhs: Iterable<T>): boolean {
+  for (const [lhsElement, rhsElement] of zipAll(lhs, rhs)) {
+    if (lhsElement === undefined) return true
+
+    if (rhsElement === undefined) return false
+
+    // @ts-ignore maybe TypeScript bug. error message: Object is possibly 'null'.
+    if (lhsElement < rhsElement) return true
+
+    // @ts-ignore maybe TypeScript bug. error message: Object is possibly 'null'.
+    if (rhsElement < lhsElement) return false
+  }
+  return true
+}
