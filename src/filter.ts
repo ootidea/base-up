01cef0d65245c2a461934459e9773d8134a75883
ootidea <ootidea@gmail.com>
2022-11-16 16:32:00 +0900

    chore: delete deplicated functions

--- src/filter.ts
+++ src/filter.ts
@@ -7,7 +7,6 @@ export function filter<T>(self: readonly T[], f: (_: T) => boolean): readonly T[
 export function filter<T>(self: readonly T[], f: (_: T) => boolean): readonly T[] {
   return self.filter(f) as any
 }
-
 export namespace filter {
   export function Iterable<T, U extends T>(self: Iterable<T>, f: (_: T) => _ is U): Generator<U>
   export function Iterable<T>(self: Iterable<T>, f: (_: T) => boolean): Generator<T>
