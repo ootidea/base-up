a9703f89bb19270e989f5938292d4bffdc0770e4
ootidea <ootidea@gmail.com>
2022-12-14 09:38:34 +0900

    setup library build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/main.ts',
+      entry: 'src/index.ts',
       formats: ['es', 'cjs'],
     },
   },
