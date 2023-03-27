73d486fa3255be519793f37831392d97d855595c
ootidea <ootidea@gmail.com>
2023-03-27 11:14:01 +0900

    fix: export more types and clamp

--- src/all.ts
+++ src/all.ts
@@ -34,7 +34,16 @@ export {
   takeWhile,
 } from './filter'
 export { zipWith, zip, zipAll, merge } from './fusion'
-export { rangeTo, rangeUpTo, repeat, repeatApply, fromEntries } from './generate'
+export {
+  rangeTo,
+  rangeUpTo,
+  repeat,
+  repeatApply,
+  fromEntries,
+  type RangeTo,
+  type RangeUpTo,
+  type RepeatArray,
+} from './generate'
 export { sumOf, groupBy } from './other'
 export { keysOf, numberKeysOf, valuesOf, entriesOf } from './projection'
 export {
@@ -88,7 +97,16 @@ export {
 } from './Set'
 export { mapOf, type NonEmptyMap, type ReadonlyNonEmptyMap } from './Map'
 export { getProperty, type NestedProperty } from './object'
-export { modOf, factorialOf, gcdOf, randomIntegerTo, randomIntegerUpTo } from './number'
+export {
+  modOf,
+  factorialOf,
+  gcdOf,
+  randomIntegerTo,
+  randomIntegerUpTo,
+  clamp,
+  type IntegerRangeTo,
+  type IntegerRangeUpTo,
+} from './number'
 export { toNumber, toString } from './string'
 export { pipe, call, identity, curry, applyFirst, bindAllButFirst, returnLast } from './Function'
 export { forever, type Promisable } from './Promise'
