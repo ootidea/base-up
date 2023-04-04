2b6b07304c2710e43073051948431bd6e038d9cf
ootidea <ootidea@gmail.com>
2023-04-04 13:08:39 +0900

    feat: use const in pipe type param

--- src/Function.ts
+++ src/Function.ts
@@ -26,12 +26,18 @@ export function returnLast<const T extends Tuple>(...args: T): T extends readonl
   return args[args.length - 1] as any
 }
 
-export function pipe<A>(a: A): A
-export function pipe<A, B>(a: A, b: (a: A) => B): B
-export function pipe<A, B, C>(a: A, b: (a: A) => B, c: (b: B) => C): C
-export function pipe<A, B, C, D>(a: A, b: (a: A) => B, c: (b: B) => C, d: (c: C) => D): D
-export function pipe<A, B, C, D, E>(a: A, b: (a: A) => B, c: (b: B) => C, d: (c: C) => D, e: (d: D) => E): E
-export function pipe<A, B, C, D, E, F>(
+export function pipe<const A>(a: A): A
+export function pipe<const A, const B>(a: A, b: (a: A) => B): B
+export function pipe<const A, const B, const C>(a: A, b: (a: A) => B, c: (b: B) => C): C
+export function pipe<const A, const B, const C, const D>(a: A, b: (a: A) => B, c: (b: B) => C, d: (c: C) => D): D
+export function pipe<const A, const B, const C, const D, const E>(
+  a: A,
+  b: (a: A) => B,
+  c: (b: B) => C,
+  d: (c: C) => D,
+  e: (d: D) => E
+): E
+export function pipe<const A, const B, const C, const D, const E, const F>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -39,7 +45,7 @@ export function pipe<A, B, C, D, E, F>(
   e: (d: D) => E,
   f: (e: E) => F
 ): F
-export function pipe<A, B, C, D, E, F, G>(
+export function pipe<const A, const B, const C, const D, const E, const F, const G>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -48,7 +54,7 @@ export function pipe<A, B, C, D, E, F, G>(
   f: (e: E) => F,
   g: (f: F) => G
 ): G
-export function pipe<A, B, C, D, E, F, G, H>(
+export function pipe<const A, const B, const C, const D, const E, const F, const G, const H>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -58,7 +64,7 @@ export function pipe<A, B, C, D, E, F, G, H>(
   g: (f: F) => G,
   h: (g: G) => H
 ): H
-export function pipe<A, B, C, D, E, F, G, H, I>(
+export function pipe<const A, const B, const C, const D, const E, const F, const G, const H, const I>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -69,7 +75,7 @@ export function pipe<A, B, C, D, E, F, G, H, I>(
   h: (g: G) => H,
   i: (h: H) => I
 ): I
-export function pipe<A, B, C, D, E, F, G, H, I, J>(
+export function pipe<const A, const B, const C, const D, const E, const F, const G, const H, const I, const J>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -81,7 +87,7 @@ export function pipe<A, B, C, D, E, F, G, H, I, J>(
   i: (h: H) => I,
   j: (i: I) => J
 ): J
-export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
+export function pipe<const A, const B, const C, const D, const E, const F, const G, const H, const I, const J, const K>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -94,7 +100,20 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
   j: (i: I) => J,
   k: (j: J) => K
 ): K
-export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
+export function pipe<
+  const A,
+  const B,
+  const C,
+  const D,
+  const E,
+  const F,
+  const G,
+  const H,
+  const I,
+  const J,
+  const K,
+  const L
+>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -108,7 +127,21 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
   k: (j: J) => K,
   l: (k: K) => L
 ): L
-export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M>(
+export function pipe<
+  const A,
+  const B,
+  const C,
+  const D,
+  const E,
+  const F,
+  const G,
+  const H,
+  const I,
+  const J,
+  const K,
+  const L,
+  const M
+>(
   a: A,
   b: (a: A) => B,
   c: (b: B) => C,
@@ -123,7 +156,7 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M>(
   l: (k: K) => L,
   m: (l: L) => M
 ): M
-export function pipe<A, T extends readonly Function[]>(a: A, ...fs: T) {
+export function pipe<const A, const T extends readonly Function[]>(a: A, ...fs: T) {
   let value: any = a
   for (const f of fs) {
     value = f(value)
