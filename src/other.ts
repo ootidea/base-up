b69b0c765d2f2cf0a291ba9272fe3e11c0e078c1
ootidea <ootidea@gmail.com>
2023-07-11 19:56:15 +0900

    feat: define groupBy.Map

--- src/other.ts
+++ src/other.ts
@@ -1,13 +1,24 @@
 import { NonEmptyArray } from './Array/MinLengthArray'
 import { push, update } from './collectionUpdate'
+import { newMap } from './Map'
 
-export function groupBy<T, U>(self: readonly T[], by: (_: T) => U): Map<U, NonEmptyArray<T>> {
-  const result = new Map<U, NonEmptyArray<T>>()
+export function groupBy<T, U extends keyof any>(self: readonly T[], by: (_: T) => U): Record<U, NonEmptyArray<T>> {
+  const result: any = {}
   for (const value of self) {
     const key = by(value)
-    update.Map.mutable(result, key, (prev) => push(prev ?? ([] as const), value))
+    result[key] instanceof Array ? result[key].push(value) : (result[key] = [value])
   }
   return result
+}
+export namespace groupBy {
+  export function Map<T, U>(self: readonly T[], by: (_: T) => U): Map<U, NonEmptyArray<T>> {
+    const result = newMap<U, NonEmptyArray<T>>()
+    for (const value of self) {
+      const key = by(value)
+      update.Map.mutable(result, key, (prev) => push(prev ?? ([] as const), value))
+    }
+    return result
+  }
 }
 
 export function sumOf(self: []): 0
