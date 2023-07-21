a8e068a8312b4ffc1e53b291f73c60a958894c64
ootidea <ootidea@gmail.com>
2023-07-22 06:06:53 +0900

    change allKeysOf return type from Set to Array

--- src/projection.test.ts
+++ src/projection.test.ts
@@ -3,38 +3,38 @@ import { assertTypeEquality } from './all'
 import { AllKeysOf, allKeysOf, allValuesOf, AllValuesOf } from './projection'
 
 test('AllKeysOf', () => {
-  assertTypeEquality<AllKeysOf<{ name: string; age: number }>, Set<'name' | 'age'>>()
-  assertTypeEquality<AllKeysOf<{ 0: string; 1: number }>, Set<'0' | '1'>>()
-  assertTypeEquality<AllKeysOf<{ [Symbol.iterator]: boolean }>, Set<typeof Symbol.iterator>>()
-  assertTypeEquality<AllKeysOf<{ name?: string; age?: number }>, Set<'name' | 'age'>>()
-  assertTypeEquality<AllKeysOf<{}>, Set<never>>()
-  assertTypeEquality<AllKeysOf<undefined>, Set<never>>()
-  assertTypeEquality<AllKeysOf<null>, Set<never>>()
+  assertTypeEquality<AllKeysOf<{ name: string; age: number }>, ('name' | 'age')[]>()
+  assertTypeEquality<AllKeysOf<{ 0: string; 1: number }>, ('0' | '1')[]>()
+  assertTypeEquality<AllKeysOf<{ [Symbol.iterator]: boolean }>, (typeof Symbol.iterator)[]>()
+  assertTypeEquality<AllKeysOf<{ name?: string; age?: number }>, ('name' | 'age')[]>()
+  assertTypeEquality<AllKeysOf<{}>, []>()
+  assertTypeEquality<AllKeysOf<undefined>, []>()
+  assertTypeEquality<AllKeysOf<null>, []>()
 
-  assertTypeEquality<AllKeysOf<{ name: string } | { age: number }>, Set<'name'> | Set<'age'>>()
+  assertTypeEquality<AllKeysOf<{ name: string } | { age: number }>, 'name'[] | 'age'[]>()
 
-  assertTypeEquality<AllKeysOf<Record<string, any>>, Set<string>>()
-  assertTypeEquality<AllKeysOf<Record<number, any>>, Set<`${number}`>>()
+  assertTypeEquality<AllKeysOf<Record<string, any>>, string[]>()
+  assertTypeEquality<AllKeysOf<Record<number, any>>, `${number}`[]>()
 
-  assertTypeEquality<AllKeysOf<{ name: string; birthday: Date }>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<Date>, Set<string | symbol>>()
+  assertTypeEquality<AllKeysOf<{ name: string; birthday: Date }>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<Date>, (string | symbol)[]>()
   class A {
     a = 1
   }
-  assertTypeEquality<AllKeysOf<A>, Set<string | symbol>>()
+  assertTypeEquality<AllKeysOf<A>, (string | symbol)[]>()
 
-  assertTypeEquality<AllKeysOf<[0, 1]>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<() => any>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<object>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<string>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<123>, Set<string | symbol>>()
-  assertTypeEquality<AllKeysOf<any>, Set<string | symbol>>()
+  assertTypeEquality<AllKeysOf<[0, 1]>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<() => any>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<object>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<string>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<123>, (string | symbol)[]>()
+  assertTypeEquality<AllKeysOf<any>, (string | symbol)[]>()
   assertTypeEquality<AllKeysOf<never>, never>()
 })
 
 test('allKeysOf', () => {
-  expect(allKeysOf({ name: 'Bob', age: 30 })).toStrictEqual(new Set(['name', 'age']))
-  expect(allKeysOf({})).toStrictEqual(new Set())
+  expect(allKeysOf({ name: 'Bob', age: 30 })).toStrictEqual(['name', 'age'])
+  expect(allKeysOf({})).toStrictEqual([])
 
   class A {
     a = 1
@@ -42,38 +42,38 @@ test('allKeysOf', () => {
   class B extends A {
     b = 2
   }
-  expect(allKeysOf(new B())).toStrictEqual(new Set(['a', 'b']))
+  expect(allKeysOf(new B())).toStrictEqual(['a', 'b'])
 
-  expect(allKeysOf({ 0: false, 1: true })).toStrictEqual(new Set(['0', '1']))
-  expect(allKeysOf({ [Symbol.iterator]: false })).toStrictEqual(new Set([Symbol.iterator]))
+  expect(allKeysOf({ 0: false, 1: true })).toStrictEqual(['0', '1'])
+  expect(allKeysOf({ [Symbol.iterator]: false })).toStrictEqual([Symbol.iterator])
 
-  expect(allKeysOf(null)).toStrictEqual(new Set())
-  expect(allKeysOf(undefined)).toStrictEqual(new Set())
+  expect(allKeysOf(null)).toStrictEqual([])
+  expect(allKeysOf(undefined)).toStrictEqual([])
 })
 
 test('AllValuesOf', () => {
-  assertTypeEquality<AllValuesOf<{ name: string; age: number }>, Set<string | number>>()
-  assertTypeEquality<AllValuesOf<{ 0: boolean; 1: bigint }>, Set<boolean | bigint>>()
-  assertTypeEquality<AllValuesOf<{ [Symbol.iterator]: boolean }>, Set<boolean>>()
-  assertTypeEquality<AllValuesOf<{ name?: string; age?: number }>, Set<string | number | undefined>>()
-  assertTypeEquality<AllValuesOf<{ name: string } | { age: number }>, Set<string> | Set<number>>()
-  assertTypeEquality<AllValuesOf<Record<number, bigint>>, Set<bigint>>()
+  assertTypeEquality<AllValuesOf<{ name: string; age: number }>, (string | number)[]>()
+  assertTypeEquality<AllValuesOf<{ 0: boolean; 1: bigint }>, (boolean | bigint)[]>()
+  assertTypeEquality<AllValuesOf<{ [Symbol.iterator]: boolean }>, boolean[]>()
+  assertTypeEquality<AllValuesOf<{ name?: string; age?: number }>, (string | number | undefined)[]>()
+  assertTypeEquality<AllValuesOf<{ name: string } | { age: number }>, string[] | number[]>()
+  assertTypeEquality<AllValuesOf<Record<number, bigint>>, bigint[]>()
 
-  assertTypeEquality<AllValuesOf<{}>, Set<never>>()
-  assertTypeEquality<AllValuesOf<undefined>, Set<never>>()
-  assertTypeEquality<AllValuesOf<null>, Set<never>>()
+  assertTypeEquality<AllValuesOf<{}>, never[]>()
+  assertTypeEquality<AllValuesOf<undefined>, []>()
+  assertTypeEquality<AllValuesOf<null>, []>()
 
-  assertTypeEquality<AllValuesOf<any>, Set<unknown>>()
-  assertTypeEquality<AllValuesOf<unknown>, Set<unknown>>()
+  assertTypeEquality<AllValuesOf<any>, any[]>()
+  assertTypeEquality<AllValuesOf<unknown>, unknown[]>()
   assertTypeEquality<AllValuesOf<never>, never>()
-  assertTypeEquality<AllValuesOf<Date>, Set<unknown>>()
-  assertTypeEquality<AllValuesOf<() => any>, Set<unknown>>()
-  assertTypeEquality<AllValuesOf<[0, 1]>, Set<unknown>>()
+  assertTypeEquality<AllValuesOf<Date>, unknown[]>()
+  assertTypeEquality<AllValuesOf<() => any>, unknown[]>()
+  assertTypeEquality<AllValuesOf<[0, 1]>, unknown[]>()
 })
 
 test('allValuesOf', () => {
-  expect(allValuesOf({ name: 'Bob', age: 30 })).toStrictEqual(new Set(['Bob', 30]))
-  expect(allValuesOf({})).toStrictEqual(new Set())
+  expect(allValuesOf({ name: 'Bob', age: 30 })).toStrictEqual(['Bob', 30])
+  expect(allValuesOf({})).toStrictEqual([])
 
   class A {
     a = 1
@@ -81,11 +81,11 @@ test('allValuesOf', () => {
   class B extends A {
     b = 2
   }
-  expect(allValuesOf(new B())).toStrictEqual(new Set([1, 2]))
+  expect(allValuesOf(new B())).toStrictEqual([1, 2])
 
-  expect(allValuesOf({ 0: false, 1: true })).toStrictEqual(new Set([false, true]))
-  expect(allValuesOf({ [Symbol.iterator]: false })).toStrictEqual(new Set([false]))
+  expect(allValuesOf({ 0: false, 1: true })).toStrictEqual([false, true])
+  expect(allValuesOf({ [Symbol.iterator]: false })).toStrictEqual([false])
 
-  expect(allValuesOf(null)).toStrictEqual(new Set())
-  expect(allValuesOf(undefined)).toStrictEqual(new Set())
+  expect(allValuesOf(null)).toStrictEqual([])
+  expect(allValuesOf(undefined)).toStrictEqual([])
 })
--- src/projection.ts
+++ src/projection.ts
@@ -1,14 +1,13 @@
-import { map } from './all'
 import { NonClassValue } from './type'
 import { Equals, IsOneOf } from './typePredicate'
 
-export type AllKeysOf<T> = IsOneOf<T, [undefined, null]> extends true
-  ? Set<never>
+export type AllKeysOf<T> = IsOneOf<T, [undefined, null, {}]> extends true
+  ? []
   : Equals<T, any> extends true
-  ? Set<string | symbol>
+  ? (string | symbol)[]
   : T extends Record<keyof any, NonClassValue>
-  ? Set<ToStringKey<keyof T>>
-  : Set<string | symbol>
+  ? ToStringKey<keyof T>[]
+  : (string | symbol)[]
 type ToStringKey<K extends keyof any> = K extends number ? `${K}` : K
 
 /**
@@ -21,35 +20,41 @@ type ToStringKey<K extends keyof any> = K extends number ? `${K}` : K
  * allKeysOf({ [Symbol.iterator]: false }) returns new Set([Symbol.iterator])
  */
 export function allKeysOf<const T>(objectLike: T): AllKeysOf<T> {
-  const result: Set<string | symbol> = new Set()
+  const resultSet: Set<string | symbol> = new Set()
+  const resultArray: Array<string | symbol> = []
 
-  if (objectLike === null || objectLike === undefined) return result as any
+  if (objectLike === null || objectLike === undefined) return resultArray as any
 
   const basePrototype = Object.getPrototypeOf({})
   let current = objectLike
   while (current !== basePrototype) {
     for (const ownPropertyName of Object.getOwnPropertyNames(current)) {
-      if (ownPropertyName !== 'constructor') {
-        result.add(ownPropertyName)
+      if (ownPropertyName === 'constructor') continue
+
+      if (!resultSet.has(ownPropertyName)) {
+        resultSet.add(ownPropertyName)
+        resultArray.push(ownPropertyName)
       }
     }
     for (const ownPropertySymbol of Object.getOwnPropertySymbols(current)) {
-      result.add(ownPropertySymbol)
+      if (!resultSet.has(ownPropertySymbol)) {
+        resultSet.add(ownPropertySymbol)
+        resultArray.push(ownPropertySymbol)
+      }
     }
     current = Object.getPrototypeOf(current)
   }
-  return result as any
+  return resultArray as any
 }
 
 export type AllValuesOf<T> = IsOneOf<T, [undefined, null]> extends true
-  ? Set<never>
+  ? []
   : Equals<T, any> extends true
-  ? Set<unknown>
+  ? any[]
   : T extends Record<keyof any, NonClassValue>
-  ? Set<T[keyof T]>
-  : Set<unknown>
+  ? T[keyof T][]
+  : unknown[]
 
 export function allValuesOf<T>(objectLike: T): AllValuesOf<T> {
-  const allKeys = allKeysOf(objectLike)
-  return map.Set(allKeys, (key) => (objectLike as any)[key]) as any
+  return allKeysOf(objectLike).map((key) => (objectLike as any)[key]) as any
 }
